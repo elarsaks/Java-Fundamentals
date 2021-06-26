9f59c63188e10224d9823a13531cf751e5e143e2
elarsaks <elarsaks@gmail.com>
2021-06-26 17:17:27 +0300

    Do Arrays.

---
+++ Arrays/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+	public static void main (String[] args) {
+		
+		int number = 10;
+		
+		//int[] numbers = new int[5];
+		int[] numbers = {15, 20, 25, 30, 35};
+		
+		System.out.println(numbers[0]); 
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]); 
+		}
+		
+	}
+}
