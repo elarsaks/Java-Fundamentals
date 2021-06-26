fa0da24666b1dcb3c6d0ef3739150728dc74a30a
elarsaks <elarsaks@gmail.com>
2021-06-26 21:20:09 +0300

    Method return types: voit, int, etc..

--- Classes/src/App.java
+++ Classes/src/App.java
@@ -1,10 +1,20 @@
 class Methods {
 	String animal;
 	int value;
+	int month;
 	
 	void run() {
 		System.out.println("Running!");
 		System.out.println("My " + animal + " is " + value + " years old.");
+	}
+	
+	int calculateMonthsToBirthday() {
+		int monthsLeft = 12 - month;
+		return monthsLeft;
+	}
+	
+	void talk(String text) {
+		System.out.println(text);
 	}
 }
 
@@ -13,7 +23,12 @@ public class App {
 		Methods name = new Methods();
 		name.animal = "cat";
 		name.value = 1;
-		
+		name.month = 8;
 		name.run();
+		
+		int months = name.calculateMonthsToBirthday();
+		System.out.println(months);
+		
+		name.talk("Hi, I am Steven!");
 	}
 }
