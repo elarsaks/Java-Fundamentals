aeff2b58f759ae860e637c1965c7cb815c4c5181
elarsaks <elarsaks@gmail.com>
2021-07-04 15:44:09 +0300

    Casting.

---
+++ Casting/src/App.java
@@ -0,0 +1,15 @@
+
+public class App {
+	public static void main(String[] args) {
+		
+		// Casting
+		byte byteValue = 3;
+		short shortValue = 38;
+		int intValue = 29;
+		long longValue = 93024;
+		float floatValue = 923.5f;
+		double doubleValue = 2341.4;
+		
+		System.out.println(Byte.MAX_VALUE);
+	}
+}
