652be7653260d8f109b43bd5830ecea1f59bb59f
elarsaks <elarsaks@gmail.com>
2021-07-18 19:53:15 +0300

    Multiple Exceptions.

---
+++ Multiple Exeptions/src/Application.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		MultipleExceptions me = new MultipleExceptions();
+		
+	/*	try {
+			me.start();
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseExeption e) {
+			System.out.println("Incorrect format");
+		} */
+	}
+
+}
